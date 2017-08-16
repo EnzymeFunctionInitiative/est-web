8e804a38328d74e890851bf78b2017a49c9fb6dd
Nils Oberg <noberg@efi.igb.illinois.edu>
2017-08-16 08:54:53 -0500

    Added language indicating required SSN version.

--- html/stepa.php
+++ html/stepa.php
@@ -66,13 +66,7 @@ $max_seq_formatted = number_format($max_seq_num, 0);
 
 </script>
 
-<!--<h3>Start With...    </h3>-->
 <h3>First step of SSN generation: Input selection</h3>
-<!--<h4>An Introduction</h4>
-<p>Start here if you are new to the &quot;Enzyme Similarity Tool&quot;.</p>
-<center><a href='index.php'><button type='button' class='css_btn_class'>Web Tutorial</button></a>
-<a href='http://dx.doi.org/10.1016/j.bbapap.2015.04.015'><button type='button' class='css_btn_class'>Review Article</button></a></center>
--->
 <br>
 Define the set of sequences to be used in the all-by-all BLAST. The similarity between the defined set of sequences will be calulated.
 Four input methods are available. A utility for SSN coloring and analysis is also available.
@@ -91,10 +85,9 @@ Four input methods are available. A utility for SSN coloring and analysis is als
     The provided sequence is used as the query for a BLAST search of the UniProt database and then, the
     similarities between the sequences are calculated and used to generate the SSN.  Submit only one
     protein sequence without FASTA header. The default maximum number of retrieved sequences is <?php echo number_format(functions::get_default_blast_seq(),0); ?>.
-<!--    Generate data set of close relatives via BLAST.  Enter only protein sequence.  Do not enter any FASTA header information. (Maximum number sequences retrieved: <?php echo number_format(functions::get_max_blast_seq(),0); ?>).</p>-->
 <fieldset id='option_a'>
 <textarea class="blast_inputs" id='blast_input' name='blast_input'><?php if (isset($_POST['blast_input'])) { echo $_POST['blast_input']; } ?></textarea>
-<p class='align_left'><a href='javascript:toggle_blast_advanced();'>Advanced Options<!-- (see tutorial)--> <span class="ui-icon ui-icon-triangle-1-e" style='display: inline-block;'></span></a></p>
+<p class='align_left'><a href='javascript:toggle_blast_advanced();'>Advanced Options<span class="ui-icon ui-icon-triangle-1-e" style='display: inline-block;'></span></a></p>
 <br><div id="blast_advanced" style="display: none;">
 <p class='align_left'>E-Value: <input type='text' class='small' id='blast_evalue' name='blast_evalue' value='<?php if (isset($_POST['blast_evalue'])) { echo $_POST['blast_evalue']; } else { echo functions::get_evalue(); } ?>'> Negative log of e-value for all-by-all BLAST (&ge; 1; default: <?php echo functions::get_evalue(); ?>)</p>
 <p class='align_left'>Maximum Blast Sequences: <input type='text' id='blast_max_seqs' class='small' name='blast_max_seqs' value='<?php if (isset($_POST['blast_max_seqs'])) { echo $_POST['blast_max_seqs']; } else { echo functions::get_default_blast_seq(); } ?>'> Maximum number of sequences retrieved (&le; <?php echo functions::get_max_blast_seq(); ?>; default: <?php echo functions::get_default_blast_seq(); ?>)</p>
@@ -114,11 +107,6 @@ Four input methods are available. A utility for SSN coloring and analysis is als
     sequences are calculated and used to generate the SSN.
     For Pfam families, the format is a comma separated list of PFxxxxx (five digits); for InterPro families, the
     format is IPRxxxxxx (six digits). The maximum number of retrieved sequences is <?php echo $max_seq_formatted; ?>.
-    <!--
-        Generate data set with Pfam and/or InterPro numbers. For Pfam families, the format is a comma separated list of
-        PFxxxxx (five digits); for InterPro families, the format is IPRxxxxxx (six digits).  The maximum number sequences
-        retrieved is <?php echo $max_seq_formatted; ?>. To identify the Pfam and/or InterPro number from a BLAST sequence, please go to <a href='<?php echo functions::get_interpro_website(); ?>' target='_blank'><?php echo functions::get_interpro_website(); ?></a>.
-    -->
 </p>
 <fieldset id='option_b'>
 <input type='text' id='families_input' name='families_input' class='blast_inputs'
@@ -126,7 +114,7 @@ Four input methods are available. A utility for SSN coloring and analysis is als
     oninput="checkFamilyInput('families_input','family_size_container','family_count_table','families_input',<?php echo $max_seq_num; ?>)"><br>
 <?php echo make_pfam_size_box('family_size_container', 'family_count_table'); ?> 
 
-<p class='align_left'><a href='javascript:toggle_pfam_advanced();'>Advanced Options<!-- (see tutorial)--><span class="ui-icon ui-icon-triangle-1-e" style='display: inline-block;'></span></a></p>
+<p class='align_left'><a href='javascript:toggle_pfam_advanced();'>Advanced Options<span class="ui-icon ui-icon-triangle-1-e" style='display: inline-block;'></span></a></p>
 <br><div id="pfam_advanced" style="display: none;">
 <p class='align_left'>E-Value: <input type='text' class='small' id='pfam_evalue' name='pfam_evalue' value='<?php if (isset($_POST['pfam_evalue'])) { echo $_POST['pfam_evalue']; } else { echo functions::get_evalue(); } ?>'> Negative log of e-value for all-by-all BLAST (&ge;1; default <?php echo functions::get_evalue(); ?>)</p>
 <p class='align_left'>Fraction: <input type='text' class='small' id='pfam_fraction' name='pfam_fraction' value='<?php if (isset($_POST['pfam_fraction'])) { echo $_POST['pfam_fraction']; } else { echo functions::get_fraction(); } ?>'>  Fraction of sequences in Pfam/Interpro family for network (&ge; 1; (default: <?php echo functions::get_fraction(); ?>)</p>
@@ -153,34 +141,17 @@ Four input methods are available. A utility for SSN coloring and analysis is als
     <b>Option C: User-supplied set of sequences</b><br>
     The similarities between the provided sequences will be calculated and used to generate the SSN.
     Input a list of protein sequences in FASTA format with headers, or upload a FASTA file.
-    <!--Generate data set with custom FASTA file. Maximum size is-->
     
     <fieldset id='option_c'>
-<!--
-        <p>
-            Input a list of protein sequences in the FASTA format with headers, and/or upload a FASTA file.
-        </p>
--->
 <?php    if (functions::option_e_enabled()) { ?>
             <p class='align_left'>
                 <input type='checkbox' id='fasta_use_headers' name='fasta_use_headers' value='1'> <b>Read FASTA headers</b><br>
                  When selected, recognized UniProt or Genbank identifiers from FASTA headers are used to retrieve corresponding node attributes from the UniProt database.
-<!--                Check to use IDs from FASTA headers to retrieve node attributes when possible (default: off).-->
             </p>
 <?php    } ?>
         <textarea class="blast_inputs" id='fasta_input' name='fasta_input'><?php if (isset($_POST['fasta_input'])) { echo $_POST['fasta_input']; } ?></textarea>
         <p>
             <?php echo make_upload_box("FASTA File", "fasta_file", "progress_bar_fasta", "progressNumberFasta"); ?>
-<!--
-            <div>
-                FASTA File:
-                <input type='file' name='fasta_file' id='fasta_file' data-url='server/php/' class="input_file">
-                <label for="fasta_file" class="file_upload"><img src="images/upload.svg" /> <span>Choose a file&hellip;</span></label>
-                <progress id='progress_bar_fasta' max='100' value='0'></progress>
-            </div>
-            <br><div id="progressNumberFasta"></div> 
-            Maximum size is <?php echo ini_get('post_max_size'); ?>b.
--->
         </p>
         <p class='align_left'>
             If desired, include a Pfam and/or InterPro families, in the analysis of your FASTA file. For Pfam families,
@@ -191,8 +162,8 @@ Four input methods are available. A utility for SSN coloring and analysis is als
             oninput="checkFamilyInput('families_input2','family_size_container_optc','family_count_table_optc','families_input2',<?php echo $max_seq_num; ?>)"><br>
 <?php echo make_pfam_size_box('family_size_container_optc', 'family_count_table_optc'); ?> 
         <p class='align_left'>
-            <a href='javascript:toggle_fasta_advanced();'>Advanced Options<!-- (see
-                tutorial)--><span class="ui-icon ui-icon-triangle-1-e" style='display: inline-block;'></span></a>
+            <a href='javascript:toggle_fasta_advanced();'>Advanced Options
+                <span class="ui-icon ui-icon-triangle-1-e" style='display: inline-block;'></span></a>
         </p>
         <br>
         <div id="fasta_advanced" style="display: none;">
@@ -226,8 +197,6 @@ Four input methods are available. A utility for SSN coloring and analysis is als
     The sequences and attributes corresponding to the recognized identifiers are retreived, and then, the similarities
     between the sequences are calculated and used to generate the SSN. Input a list of Uniprot, NCBI, or Genbank
     sequence accession IDs, or upload a text file containing the accession IDs. 
-    <!--Generate data set from a file with a list of Uniprot, NCBI, or Genbank sequence accession IDs.
-    Maximum size is <?php echo ini_get('post_max_size'); ?>b.-->
     <fieldset id='option_d'>
         <p class='align_left'>
             Input a list of Uniprot, NCBI, or Genbank sequence accession IDs, and/or upload a text file containing
@@ -236,12 +205,6 @@ Four input methods are available. A utility for SSN coloring and analysis is als
         <textarea class="blast_inputs" id='accession_input' name='accession_input'><?php if (isset($_POST['accession_input'])) { echo $_POST['accession_input']; } ?></textarea>
         <p>
             <?php echo make_upload_box("Accession ID File", "accession_file", "progress_bar_accession", "progressNumberAccession"); ?>
-<!--
-            Accession ID File: <input type='file' name='accession_file' id='accession_file' data-url='server/php/'>
-            <progress id='progress_bar_accession' max='100' value='0'></progress>
-            <br><div id="progressNumberAccession"></div>
-            Maximum size is <?php echo ini_get('post_max_size'); ?>b.
--->
         </p>
         <p class='align_left'>
             If desired, include a Pfam and/or InterPro families, in the analysis of your file. For Pfam families,
@@ -252,8 +215,8 @@ Four input methods are available. A utility for SSN coloring and analysis is als
             oninput="checkFamilyInput('families_input4','family_size_container_optd','family_count_table_optd','families_input4',<?php echo $max_seq_num; ?>)"><br>
 <?php echo make_pfam_size_box('family_size_container_optd', 'family_count_table_optd'); ?> 
         <p class='align_left'>
-            <a href='javascript:toggle_accession_advanced();'>Advanced Options<!--(see 
-                tutorial)--><span class="ui-icon ui-icon-triangle-1-e" style='display: inline-block;'></span></a>
+            <a href='javascript:toggle_accession_advanced();'>Advanced Options
+                <span class="ui-icon ui-icon-triangle-1-e" style='display: inline-block;'></span></a>
         </p>
         <br>
         <div id="accession_advanced" style="display: none;">
@@ -288,66 +251,11 @@ Four input methods are available. A utility for SSN coloring and analysis is als
     <input type='radio' id='option_selected_colorssn' name='option_selected' value='colorssn' onChange='disable_forms();'>
     <b>Color SSN Utility: Color a previously generated SSN and return associated cluster data.</b><br>
     Independent sequence clusters in the uploaded SSN are identified, numbered and colored. Summary tables,
-    sets of IDs and sequences for specific clusters are provided. A Cytoscape-edited SNN can serve as input for this utility. 
-    <!--Color an input SSN and return associated cluster data.-->
+    sets of IDs and sequences for specific clusters are provided. A Cytoscape-edited SNN can serve as input for this utility.
+    In order for all of the new features to work correctly, SSNs generated after 8/16/2017 should be used. 
     <fieldset id='option_colorssn'>
         <p>
             <?php echo make_upload_box("SNN to color and analyze (uncompressed or zipped XGMML file)", "colorssn_file", "progress_bar_colorssn", "progressNumberColorSsn"); ?>
-<!--
-            SNN to color and analyze (uncompressed or zipped XGMML file):
-            <input type='file' name='colorssn_file' id='colorssn_file' data-url='server/php/'>
-            <progress id='progress_bar_colorssn' max='100' value='0'></progress>
-            <br>
-            <div id="progressNumberColorSsn"></div>
--->
-            Maximum size is <?php echo ini_get('post_max_size'); ?>b.
-<?php /* ?>
-            <p>
-                Neighborhood Size (default: <?php echo $default_neighbor_size; ?>)
-                <select name='neighbor_size' id='neighbor_size'>
-                    <?php echo $neighbor_size_html; ?>
-                </select>
-            </p>
-            <p>
-                <label for='cooccurrence_input'>
-                    Input % Co-Occurrence Lower Limit (Default: <?php echo functions::get_default_cooccurrence(); ?>, Valid 1-100):
-                </label>
-                <input type='text' id='cooccurrence' name='cooccurrence' maxlength='3' value='<?php echo functions::get_default_cooccurrence(); ?>'><br>
-            </p>
-            <p>
-                If desired, include a Pfam and/or InterPro families, in the analysis of your file. For Pfam families,
-                the format is a comma separated list of PFxxxxx (five digits); for InterPro families, the format is 
-                IPRxxxxxx (six digits).
-            </p>
-            <input type='text' id='families_input4' name='families_input4' class='blast_inputs' value='<?php if (isset($_POST['families_input4'])) { echo $_POST['families_input4']; } ?>'>
-            <p class='align_left'>
-                <a href='javascript:toggle_accession_advanced();'>Advanced Options (see
-                tutorial)<span class="ui-icon ui-icon-triangle-1-e" style='display: inline-block;'></span></a>
-            </p>
-            <br>
-            <div id="accession_advanced" style="display: none;">
-                <p class='align_left'>
-                    E-Value: 
-                    <input type='text' class='small' id='accession_evalue' name='accession_evalue' value='<?php if (isset($_POST['accession_evalue'])) { echo $_POST['accession_evalue']; } else { echo functions::get_evalue(); } ?>'>
-                    Negative log of e-value for all-by-all BLAST (&ge;1; default: <?php echo functions::get_evalue(); ?>)
-                </p>
-                <p class='align_left'>
-                    Fraction:
-                    <input type='text' class='small' id='accession_fraction' name='accession_fraction' value='<?php if (isset($_POST['accession_fraction'])) { echo $_POST['accession_fraction']; } else { echo functions::get_fraction(); } ?>'>
-                    Fraction of sequences in Pfam/Interpro family for network (&ge; 1; default: <?php echo functions::get_fraction(); ?>)
-                </p>
-<?php    if (functions::get_program_selection_enabled()) { ?>
-                <p class='align_left'>Select Program to use:
-                    <select name='option_d_program' id='option_d_program'>
-                        <option value='BLAST'>Blast</option>
-                        <option value='BLAST+'>Blast+</option>
-                        <option selected='selected' value='DIAMOND'>Diamond</option>
-                	    <option value='DIAMONDSENSITIVE'>Diamond Sensitive</option>
-                    </select>
-                </p>
-<?php    } ?>
-            </div>
-<?php */ ?>
         </p>
     </fieldset>
 </p>
